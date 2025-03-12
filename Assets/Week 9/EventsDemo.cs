d710f85e3112ceacba24ba198eea858607018a34
Jiawei Yu <yujiawe@shernet.sheridancollege.ca>
2025-03-12 11:05:04 -0400

    Week 9 coding gym

---
+++ Assets/Week 9/EventsDemo.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventsDemo : MonoBehaviour
+{
+    public RectTransform banana;
+    public UnityEvent OnTimerFinished;
+    public float timerLength = 2;
+    public float t;
+
+    private void Update()
+    {
+        t += Time.deltaTime;
+        if (t > timerLength)
+        {
+            OnTimerFinished.Invoke();
+            t = 0;
+        }
+        //if (t < timerLength)
+        //{
+        //    t += Time.deltaTime;
+        //}
+        //else
+        //{
+        //    OnTimerFinished.Invoke();
+        //}
+    }
+
+    public void MouseJustEntered()
+    {
+        Debug.Log("The mouse just entered me!");
+        banana.localScale = Vector3.one * 1.2f;
+    }
+    public void MouseJustLeft()
+    {
+        Debug.Log("The mouse just left me!");
+        banana.localScale = Vector3.one;
+    }
+}
